30e77a14bb1ed263b5d09b74802dc226406eac9d
Lucas Olmos <lucolmos@gmail.com>
2020-04-26 13:55:58 -0300

    Se añade un cambio

--- ConsoleAppNetCore/ConsoleAppNetCore/Program.cs
+++ ConsoleAppNetCore/ConsoleAppNetCore/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Lucas!");
         }
     }
 }
